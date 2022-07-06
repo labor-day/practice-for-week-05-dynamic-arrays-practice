a3659ea0b94b974634a35db61dd5e7153daaef72
labor-day <shaffer.scott@yahoo.com>
2022-07-05 18:25:46 -0600

    complete

--- solution/dynamic-array.js
+++ solution/dynamic-array.js
@@ -3,17 +3,24 @@ class DynamicArray {
   constructor(defaultSize=4) {
 
     // Your code here
+    this.data = new Array(defaultSize);
+    this.length = 0;
+    this.capacity = defaultSize;
 
   }
 
   read(index) {
-
-    // Your code here
+    return this.data[index];
   }
 
   unshift(val) {
-
-    // Your code here
+    let bigger = new Array(this.capacity+1);
+    for (let i = this.data.length - 1; i >= 0; i--) {
+      bigger[i+1] = this.data[i];
+    }
+    bigger[0] = val;
+    this.data = bigger;
+    this.length++;
   }
 
 }
